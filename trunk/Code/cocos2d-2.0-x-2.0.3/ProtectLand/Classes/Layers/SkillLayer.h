2a0a86ae4349f1c76df07dcdb8c1936554b066f0
phuctri9898 <phuctri9898@b2c73e45-1013-c018-4b3d-7bf9259b0fa6>
2012-11-29 02:59:04 +0000

    skill layer

--- trunk/Code/cocos2d-2.0-x-2.0.3/ProtectLand/Classes/Layers/SkillLayer.h
+++ trunk/Code/cocos2d-2.0-x-2.0.3/ProtectLand/Classes/Layers/SkillLayer.h
@@ -4,8 +4,10 @@
 #include "cocos2d.h"
 
 USING_NS_CC;
-
-
+#define SKILL_1 1 // minus half of hp of all monster
+#define SKILL_2 2 // kill all fire monster
+#define SKILL_3 3 // kill all water monster
+#define SKILL_4 4 // kill all monster
 class CSkillLayer: public cocos2d::CCLayer
 {
 private:	
@@ -14,13 +16,24 @@ private:
 	CCSprite *m_sprite3;
 	CCSprite *m_sprite4;
 	CCSprite *m_sprite5;
+	int stt;
+	int resultSkill;
 	float m_timer;
+	//bool arr_isTouched[5];
+	int arr_PointTouched[6];
 public:
 	CSkillLayer();
 	~CSkillLayer();
 	virtual bool init();
 	virtual void onEnter();
 	void addSphere(CCSprite *m_sprite,char *path,int x,int y);
+	bool inAreaSphere(int x, int y, CCPoint &p);
+	void initVar();
+	void checkClick(CCPoint &p);
+	int pow(int x,int n);
+	void calculateSkill();
+	int getResultSkill();
+	void removeLayer();
 	virtual void onExit();
 	virtual void update(float dt);
 	virtual bool ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent);
